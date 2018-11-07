036c649c953164ce1753ce307407c25b2c316910
jr36 <jrrr0004@hotmail.com>
2018-11-07 12:29:03 +0100

    Sesion 3
    Cambios en soporte al comando HELO, añado DATA, RCPT, MAIL FROM, y mensaje, aun no funcionan

--- cliente.c
+++ cliente.c
@@ -31,7 +31,7 @@ int main(int *argc, char *argv[])
 	struct sockaddr *server_in = NULL; //Crea la estructura del socket y la añade al servidor
 	struct sockaddr_in server_in4;
 	int address_size = sizeof(server_in4);
-	char buffer_in[1024], buffer_out[1024], input[1024];// Es el tamaño maximo de 1024 del buffer de los datos recibidos, enviados y los que entran.
+	char buffer_in[1024], buffer_out[1024], input[1024], to[20], from[20], subject[20];// Es el tamaño maximo de 1024 del buffer de los datos recibidos, enviados y los que entran.
 	int recibidos = 0, enviados = 0;
 	int estado = S_HELO;//Primer valor en la variable estado ya que será una maquina de estados
 	char option;
@@ -73,21 +73,17 @@ int main(int *argc, char *argv[])
 			gets_s(ipdest, sizeof(ipdest));//obtiene la ip de destino en la variable ipdest y almacena en la variable
 
 			//Dirección por defecto según la familia
-			if (strcmp(ipdest, "") == 0 && ipversion == AF_INET)
+			if (strcmp(ipdest, "") == 0) // Compara con una cadena de caracteres vacia y al ser enter una cadena de caracteres vacia se cumple y copia la de default en la ipdest
 				strcpy_s(ipdest, sizeof(ipdest), default_ip4);
 
-		
 
-			if (ipversion == AF_INET) {
-				server_in4.sin_family = AF_INET;
-				server_in4.sin_port = htons(TCP_SERVICE_PORT);
-				//server_in4.sin_addr.s_addr=inet_addr(ipdest);
-				inet_pton(ipversion, ipdest, &server_in4.sin_addr.s_addr);
-				server_in = (struct sockaddr*)&server_in4;
-				address_size = sizeof(server_in4);
-			}
+			server_in4.sin_family = AF_INET;
+			server_in4.sin_port = htons(TCP_SERVICE_PORT);
+			//server_in4.sin_addr.s_addr = inet_addr(ipdest);//Para meter una ip de manera numerica
+			inet_pton(ipversion, ipdest, &server_in4.sin_addr.s_addr);
+			server_in = (struct sockaddr*)&server_in4;
+			address_size = sizeof(server_in4);
 
-			
 			estado = S_HELO;//Asigna estado a S_HELO
 
 			//PRIMITIVA CONNECT
@@ -98,94 +94,201 @@ int main(int *argc, char *argv[])
 				recibidos = recv(sockfd, buffer_in, 512, 0); //recibimos la informacion del servidor para saber que esta listo
 				buffer_in[recibidos] = 0x00;
 				printf(buffer_in);
-
 				//Inicio de la máquina de estados
-				do{
-					switch(estado){
-					
+				do {
+					switch (estado) {
 					case S_HELO:
-
-						sprintf_s(buffer_out, sizeof(buffer_out), "HELO %s%s", ipdest, CRLF);//Mensaje inicial HELO
-						
-						break;
-					case S_USER:
-						// establece la conexion de aplicacion 
-						printf("CLIENTE> Introduzca el usuario (enter para salir): ");
-						gets_s(input,sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
-
-						sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",SC,input,CRLF);
-						break;
-					case S_PASS:
-						printf("CLIENTE> Introduzca la clave (enter para salir): ");
+						printf("CLIENTE> Para comenzar, ingrese el comando HELO: ");
 						gets_s(input, sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",PW,input,CRLF);
+						
+							sprintf_s(buffer_out, sizeof(buffer_out), "HELO %s%s", ipdest, CRLF);//Mensaje inicial HELO
+						
+
 						break;
+					case S_MAIL_FROM:
+
+							printf("CLIENTE> MAIL FROM: (Pulse intro para salir) ");
+							gets_s(input, sizeof(input));
+							if (strlen(input) == 0) {
+								// Si la longitud de input es 0, 
+								//Escribe en el servidor QUIT y pasamos al estado QUIT
+								sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", "QUIT", CRLF);
+								estado = S_QUIT;
+							}
+							else {
+									sprintf_s(buffer_out, sizeof(buffer_out), "MAIL FROM:<%s>%s", input, CRLF);//MENSAJE DE REMITENTE
+									strcpy_s(from,sizeof(from), input);//guardamos para el mensaje completo
+								}
+
+
+						break;
+					case S_RCPT_TO:
+							
+							printf("CLIENTE> RCPT TO: (Pulse intro para salir) ");
+							gets_s(input, sizeof(input));
+
+							if (strlen(input) == 0) {
+								// Si la longitud de input es 0, 
+								//Escribe en el servidor QUIT y pasamos al estado QUIT
+								sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", "QUIT", CRLF);
+								estado = S_QUIT;
+							}
+							else {
+
+								sprintf_s(buffer_out, sizeof(buffer_out), "RCPT TO:<%s>%s", input, CRLF);//enviamos el destinatario
+								strcpy_s(to,sizeof(to), input);
+							}
+						
+							break;
+
 					case S_DATA:
-						printf("CLIENTE> Introduzca datos (enter o QUIT para salir): ");
-						gets_s(input, sizeof(input));
-						if(strlen(input)==0){
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s%s",SD,CRLF);
-							estado=S_QUIT;
-						}
-						else
-							sprintf_s (buffer_out, sizeof(buffer_out), "%s %s%s",ECHO,input,CRLF);
+						sprintf_s(buffer_out, sizeof(buffer_out), "DATA%s", CRLF);//comando para pasar al mensaje
+						estado++;
 						break;
-				
-					}
+					
+					case S_MENSAJE:
+						printf("CLIENTE> subject: ");
+						gets(input);
+						strcpy_s(subject,sizeof(subject), input);
+						sprintf_s(buffer_out, sizeof(buffer_out), "Asunto: %s%sDe:%s%A: %s%s", subject, CRLF, to, CRLF, from, CRLF);//enviamos las cabeceras
 
-					if(estado!=S_HELO){
-						enviados=send(sockfd,buffer_out,(int)strlen(buffer_out),0);
-						if(enviados==SOCKET_ERROR){
-							 estado=S_QUIT;
-							 continue;
-						}
-					}
-						
-					recibidos=recv(sockfd,buffer_in,512,0);
-					if(recibidos<=0){
-						DWORD error=GetLastError();
-						if(recibidos<0){
-							printf("CLIENTE> Error %d en la recepción de datos\r\n",error);
-							estado=S_QUIT;
-						}
-						else{
-							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
-							estado=S_QUIT;
-						}
-					}else{
-						buffer_in[recibidos]=0x00;
-						printf(buffer_in);
-						if(estado!=S_DATA && strncmp(buffer_in,OK,2)==0) 
-							estado++;  
-					}
+						do {//vamos montando un mensaje en el buffer out hasta que sea distinto de un .
+							printf("Redacte su mensaje: (Si desea terminar teclee '.'):", to, CRLF);
+							gets(input);
+							sprintf_s(buffer_out, sizeof(buffer_out), "%s%s%s", buffer_out, CRLF, input);
+							fflush(stdin);
+							fflush(stdout);
+						} while (strcmp(input, ".") != 0);
+						sprintf_s(buffer_out, sizeof(buffer_out), "%s%s", buffer_out, CRLF);
+
+						break;
+
+
+
+
 					case S_QUIT:
 						/*estado para Cerrar conexión*/
 						break;
 
-				}while(estado!=S_QUIT);		
-			}
-			else{
-				int error_code=GetLastError();
-				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n",ipdest,TCP_SERVICE_PORT);
-			}		
-			// fin de la conexion de transporte
-			closesocket(sockfd);
-			
-		}	
-		printf("-----------------------\r\n\r\nCLIENTE> Volver a conectar (S/N)\r\n");
-		option=_getche();
 
-	}while(option!='n' && option!='N');
+					}
+
+
+					//**************************************************************************************************************************
+
+					//Envio
+					
+
+					enviados = send(sockfd, buffer_out, (int)strlen(buffer_out), 0);
+			
+					/* vamos ha realizar una comprobacion de los datos enviados muy parecida a lo que se hace en recibidos*/
+					if (enviados <= 0) {// si los datos enviados tienen un valor de menor o igual que 0
+						DWORD error = GetLastError();
+						if (enviados < 0) { // si los datos enviados tienen un valor de -1 hay error
+							printf("SERVIDOR> Error %d en el envio de datos\r\n", error);
+							estado = S_QUIT;
+							continue;
+						}
+						else { // si no, es decir, si su valor es 0 se cierra la conexion
+							printf("SERVIDOR> Conexión con el cliente cerrada\r\n");
+							estado = S_QUIT;
+							continue;
+						}
+					}
+					else { // el valor es mayor que 0 y por tanto los datos se envian correctamente
+						//printf("SERVIDOR> Datos enviados correctamente\r\n");
+					}
+					//}
+
+					//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-	
+
+
+
+
+					//Recibo
+
+
+					recibidos = recv(sockfd, buffer_in, 512, 0);
+
+					if (recibidos <= 0) {
+						DWORD error = GetLastError();
+						if (recibidos < 0)
+						{
+							printf("CLIENTE> Error %d en la recepción de datos\r\n", error);
+							estado = S_QUIT;
+							continue;
+						}
+						else
+						{
+							printf("CLIENTE> Conexión con el servidor cerrada\r\n");
+							estado = S_QUIT;
+							continue;
+
+
+						}
+					}
+					else
+					{
+						buffer_in[recibidos] = 0x00;
+						printf(buffer_in);
+
+						switch (estado) {
+						case S_HELO:
+							if (estado != S_QUIT) {
+								estado++;
+							}
+							else estado = S_QUIT;
+							break;
+						case S_MAIL_FROM:
+							if (estado != S_QUIT) {
+								estado++;
+							}
+							else estado = S_QUIT;
+							break;
+						case S_RCPT_TO:
+							if (estado != S_QUIT) {
+								estado++;
+								}
+							else estado = S_QUIT;
+							break;
+
+							/*Las siguientes lineas de codigo lo utilizamos para enviar mas mensajes antes de finalizar la sesion*/
+						case S_MENSAJE:
+							if (buffer_in[0] == '2') {
+								printf("¿Quiere mandar otro mensaje antes de finalizar sesion?(si/no)\r\n");
+								if (_getch() == 's') {
+									estado = S_MAIL_FROM;
+								}
+								else { estado = S_QUIT; }
+								break;
+
+
+							}
+						}
+
+					}
+				} while (estado != S_QUIT);
+
+
+
+
+
+			}
+			else
+			{
+				printf("CLIENTE> ERROR AL CONECTAR CON %s:%d\r\n", ipdest, TCP_SERVICE_PORT);
+			}
+			// fin de la conexion de transporte
+			closesocket(sockfd);//cerramos el socket
+
+		}
+		printf("-----------------------\r\n\r\nCLIENTE> Volver a conectar (S/N)\r\n");
+		option = _getche();
+
+	} while (option != 'n' && option != 'N');
+
+
 
 	return(0);
+
 }
--- protocol.h
+++ protocol.h
@@ -4,12 +4,7 @@
 
 // COMANDOS DE APLICACION
 #define HE "HELO"
-#define SC "USER"  // SOLICITUD DE CONEXION USER usuario 
-#define PW "PASS"  // Password del usuario  PASS password
-
-#define SD  "QUIT"  // Finalizacion de la conexion de aplicacion
-#define SD2 "EXIT"  // Finalizacion de la conexion de aplicacion 
-#define ECHO "ECHO" // Definicion del comando "ECHO" para el servicio de eco
+#define DATA "DATA"
 
 
 // RESPUESTAS A COMANDOS DE APLICACION
@@ -21,15 +16,13 @@
 
 //ESTADOS
 #define S_HELO 0
-#define S_USER 1
-#define S_PASS 2
+#define S_MAIL_FROM 1
+#define S_RCPT_TO 2
 #define S_DATA 3
-#define S_QUIT 4
-#define S_EXIT 5
+#define S_MENSAJE 4
+#define S_QUIT 5
+#define S_EXIT 6
 
 //PUERTO DEL SERVICIO
 #define TCP_SERVICE_PORT	25
 
-// NOMBRE Y PASSWORD AUTORIZADOS
-#define USER		"alumno"
-#define PASSWORD	"123456"
